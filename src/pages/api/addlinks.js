a17a2ce2842a9dbfba4725b4eec4ff3e1e1d04f4
Joel Ajay <ajaijoel@gmail.com>
2023-05-01 21:34:10 +0530

    Added file to implement route for adding New Links

---
+++ src/pages/api/addlinks.js
@@ -0,0 +1,30 @@
+import admin from "lib/firestoreAdmin.js";
+
+export default async function handler(req, res) {
+    
+    if(req.method == "POST"){
+        // Get all links
+        
+        const db = admin.firestore();
+
+        var myLinks = [];
+        
+        const usersRef = db.collection("users");
+        
+        const querySnapshot = await usersRef.where('username', '==', req.body.username).get();
+        if (querySnapshot.empty) {
+            console.log("username does not exist");
+            res.status(404).json({ links: [] });;
+            return;
+        } else {
+            const linksRef = querySnapshot.docs[0].ref.collection("links");
+            const links = await linksRef.get();
+            links.forEach(doc => {
+                myLinks.push(doc.data());
+            });
+            res.status(200).json({ links: myLinks });
+        }
+        
+    }
+
+}
