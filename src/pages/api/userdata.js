5fa8e3f4b142c07b8a37eac29f4831e1d3113cf1
Joel Ajay <ajaijoel@gmail.com>
2023-05-12 21:40:33 +0530

    Added GET method to get userdata before POST req

--- src/pages/api/userdata.js
+++ src/pages/api/userdata.js
@@ -1,18 +1,23 @@
 import admin from "lib/firestoreAdmin.js";
 
 export default async function handler(req, res) {
+    const db = admin.firestore();
+    const usersRef = db.collection("users");
 
     if(req.method == "GET"){
-        
+        // Get all usernames existing
+        let usersList = [];
+        const users = await usersRef.get();
+        users.forEach(userDoc => {
+           usersList.push(userDoc.data());
+        });
+        console.log(usersList);
+        res.status(200).json({users: usersList});
     }
     
     if(req.method == "POST"){
         // Add username to users database
-
         
-        const db = admin.firestore();
-        
-        const usersRef = db.collection("users");
         const usernameQuerySnapshot = await usersRef.where('username', '==', req.body.username).get();
         if (!usernameQuerySnapshot.empty) {
             console.log("username already exists");
@@ -25,7 +30,7 @@ export default async function handler(req, res) {
             res.status(404).json({message: "Not found"});
             return;
         } else {
-            const userDocRef = await querySnapshot.docs[0].ref.set({
+            await querySnapshot.docs[0].ref.set({
                 username: req.body.username,
               }, 
               { merge: true });
