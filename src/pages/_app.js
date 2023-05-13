1999572a7168b173d7667f4cb1c2b229cf98ac6e
Joel Ajay <ajaijoel@gmail.com>
2023-05-13 10:36:05 +0530

    Removed user-defined css in favour of Material UI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import { SessionProvider } from "next-auth/react"
-import '@/styles/globals.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
