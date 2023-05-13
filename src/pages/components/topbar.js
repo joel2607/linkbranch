0c9546bd0bab6838b08643fa530a1e4fe0a28f55
Joel Ajay <ajaijoel@gmail.com>
2023-05-13 08:33:06 +0530

    Added UI to index page

--- src/pages/components/topbar.js
+++ src/pages/components/topbar.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { signIn, signOut} from 'next-auth/react';
 
 import AppBar from '@mui/material/AppBar';
@@ -9,13 +8,15 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import Avatar from '@mui/material/Avatar';
+import { Tooltip } from '@mui/material';
 
-export default function Topbar({session}){
+export default function Topbar({session, username}){
 
-  // function createAccount(username){
+  function handleSignIn(username){
 
 
-  // }
+    signIn('google');
+  }
 
     return (
     <Box sx={{ flexGrow: 1 }}>
@@ -27,6 +28,7 @@ export default function Topbar({session}){
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            href="\"
           >
             <AccountTreeIcon />
           </IconButton>
@@ -34,19 +36,19 @@ export default function Topbar({session}){
             LinkBranch
           </Typography>
 
-          
+          <Tooltip text = {(session.data)?session.data.user.name:""}>
             <Avatar 
               src = {(session.data)?session.data.user.image:""} 
-              height = "50" width = "50"
               sx={{
               display: session.data?"block":"none",
               mr: 2
               }}
               alt='User Profile Picture'></Avatar>
-            
+          
+          </Tooltip>
           <Button onClick={() => signOut()} sx = {{display: session.data?"block":"none"}}>Sign out</Button>
 
-          <Button color="inherit" onClick={() => signIn('google')} sx = {{display: (!session.data)?"block":"none"}} >Login</Button>
+          <Button color="inherit" onClick={() => handleSignIn()} sx = {{display: (!session.data)?"block":"none"}} >Login </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,21 @@ import Head from 'next/head'
 import {useSession } from 'next-auth/react';
 
 import Userform from './components/UserForm';
-import Topbar from './components/topbar';
-import LinkAdd from './components/linkAdd';
+import Topbar from './components/Topbar';
+import MainPageBody from './components/MainPageBody';
+import { useState } from 'react';
 
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 export default function Home() {
 
   const session = useSession();
+  const [username, setUsername] = useState("");
   return ( 
 
     <>
@@ -21,7 +29,43 @@ export default function Home() {
       </Head>
 
       <Topbar session={session}></Topbar>
-      <LinkAdd session = {session}></LinkAdd>
+      
+      
+      
+      <Card sx={{
+        position: "relative",
+        display: (session.data)?"none":"flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        margin: "10px 10px 10px 10px",
+        height: "88vh",
+      }}>
+        <CardContent sx = {{ position:"absolute"}}>
+
+          {/* <Typography variant = "h4">
+          Welcome to LinkBranch<br/><br/>
+          </Typography>  */}
+
+          <Typography variant = "h6" sx={{fontWeight:"normal"}}>
+            Sign in to claim your domain at 
+          </Typography>
+
+          <TextField
+            label="/your-username"
+            value={username}
+            size='small'
+            variant='standard'
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+
+        </CardContent>
+      </Card>
+
+      <MainPageBody session = {session}></MainPageBody>
       <Userform session = {session}></Userform>
       
     </>
